4da6ea33fee1a7d44e36b443657130752d9e6bf8
Сергей <Resler18@yandex.com>
2024-11-26 00:43:24 +0300

    fqa acarddion list

--- vite-project/src/pages/faq.jsx
+++ vite-project/src/pages/faq.jsx
@@ -13,6 +13,11 @@ const Faq = () => {
   let active = isActive ? 'accordion-inactive' : 'accordion-active';
   let activeBlock = isActive ? 'faq_block_second_inactive' :  'faq_block_second'; 
 
+  const items = [
+    {id : 1, content : 'Ответ: Мы предоставляем опции для анонимной регистрации и общения. Ваша личная информация остается защищенной, и вы решаете, что и когда делиться с другими пользователями.'},
+
+  ];
+
   return (
     <>
       <div className="container">
@@ -23,9 +28,13 @@ const Faq = () => {
           </div>
           <div className={activeBlock}> 
             <span > Я беспокоюсь о своей приватности. Как вы обеспечиваете анонимность на сайте?</span>
-               <p className={active}>
-               Ответ: Мы предоставляем опции для анонимной регистрации и общения. Ваша личная информация остается защищенной, и вы решаете, что и когда делиться с другими пользователями.
-                </p> 
+                  {
+                    items.map(({id, content}) => {
+                      return (
+                      <p key={id} className={active}>{content}</p>
+                      )
+                    })
+                  }
             <img onClick={handleToggle} className='icon_1' src={icon_2} alt="icon_2" />
           </div>
         </div>
